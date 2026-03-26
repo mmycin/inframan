943bc72284e4be3ab1df235a4e8470636fe476d8
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2026-03-26 17:59:29 +0600

    TFile Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "CLI11.hpp"
 #include "app-config.hpp"
+#include "tfile.h"
 
 using namespace std;
 using namespace CLI;
@@ -8,6 +9,6 @@ int main(int argc, char** argv) {
     App app{AppConfig::name, AppConfig::description};
     argv = app.ensure_utf8(argv);
 
-    CLI11_PARSE(app, argc, argv);
+    CLI11_PARSE(app, argc, argv); 
     return 0;
 }
