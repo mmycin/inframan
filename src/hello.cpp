be0a9c51ca8fc4480c70abd720e9ca5f8780e5d3
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2026-03-26 13:47:40 +0600

    CLI11 Added

--- src/hello.cpp
+++
@@ -1,7 +0,0 @@
-#include "hello.hpp"
-
-using namespace hello;
-
-void Hello::say_hello() {
-    std::cout << "Hello, World!" << std::endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
-#include "hello.hpp"
+#include "CLI11.hpp"
 
-using namespace hello;
+using namespace std;
 
-int main() 
-{
-	Hello::say_hello();
+int main(int argc, char** argv) {
+    CLI::App app{"InfraManager"};
+    argv = app.ensure_utf8(argv);
+    
+    string filename = "default";
+    app.add_option("-f,--file", filename, "Input file");
+
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
